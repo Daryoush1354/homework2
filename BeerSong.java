21c28fcc6fd16e69136cffabf5721a1e4b71854e
Daryoush Nasiri <dnasiri@oakland.edu>
2016-09-28 18:00:35 -0400

    I added pseudocode for BeerSong.java

---
+++ BeerSong.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import javax.swing.*;
+public class BeerSong{
+public static void main(String[] args) {
+		
+		Ninety_Nine_Bottles_Of_Beers();
+
+	}
+public static void Ninety_Nine_Bottles_Of_Beers(){
+
+			i need loop from 99 to 2 and shows the message of bottles of beer and take one down
+			and give it to next bottle.
+			when the variable is equal 1 shows the different message.
+				}
+					
+			
+			
+			
